35c6480b0bf46b39e56a922a42cf88cc7ace373e
Eungyeol41 <iniziato@naver.com>
2021-09-09 02:39:22 +0900

    are 추가

---
+++ react-are/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Detail from "./comps/Detail";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>BOX ATELIER</h2>
+        <p>With. RE;CODE</p>
+      </header>
+      <Detail />
+    </div>
+  );
+}
+
+export default App;
