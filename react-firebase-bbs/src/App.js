7861d69e6ac8334f19dd3cdb2314d504c0c27439
Eungyeol41 <iniziato@naver.com>
2021-09-09 14:17:02 +0900

    gitignore 수정

--- react-firebase-bbs/src/App.js
+++ react-firebase-bbs/src/App.js
@@ -7,15 +7,19 @@ import Write from "./comps/BBsWrite";
 
 import { BrowserRouter, Route } from "react-router-dom";
 import Header from "./comps/Header";
+import Footer from "./comps/Footer";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Header />
+
         <MainNav />
         <Route path="/" component={Main} exact />
         <Route path="/write" component={Write} exact />
+        <section className="main_section"></section>
+        <Footer />
       </div>
     </BrowserRouter>
   );
---
+++ react-firebase-bbs/src/comps/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <address>CopyRight &copy;inizato@naver.com</address>
+    </footer>
+  );
+}
+
+export default Footer;
